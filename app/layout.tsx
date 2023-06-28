5cd5f464fd425bfdfb097c7cf71d380be409765c
Toyman-tech <adiguno204@gmail.com>
2023-06-28 03:02:04 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
+import './globals.css'
+// import { Inter } from 'next/font/google'
+
+// const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Flexible',
+  description: 'Showcase and discover remarkable developers projects',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+      <Navbar/>
+      <main>  
+        {children}
+      </main>
+      <Footer/>
+
+
+
+      </body>
+    </html>     
+  )
+}
